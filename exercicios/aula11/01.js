929cda5109c7cc0bb72707ecd5ae2c07443407d9
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-02-28 00:25:32 -0300

    estudando

--- exercicios/aula11/01.js
+++ exercicios/aula11/01.js
@@ -1,2 +1,6 @@
-var vel = 65.5
+var vel = 40.5
 console.log(`a velocidade do seu carro é ${vel} km/h `)
+if (vel > 80) { //condiçao simples
+            console.log('vc passou do limite de velocidade. MULTADO!')
+}
+console.log('dirija sempre usando cinto de segurança.')
---
+++ exercicios/aula11/02.js
@@ -0,0 +1,11 @@
+var pais = 'frança'
+console.log(`vivendo em ${pais}`)
+
+if (pais == 'brasil') {
+    console.log('vc é brasileiro')
+} else {
+    console.log('vc é estrangeiro')
+}
+
+
+
