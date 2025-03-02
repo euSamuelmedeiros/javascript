e201475e4864edfbe83e57ca5c31eedbca8b3573
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-03-02 17:28:12 -0300

    desafio pronto

--- desafios/001/script.js
+++ desafios/001/script.js
@@ -1,1 +1,26 @@
-window.alert('Ola! Seja bem vindo(a) ao nosso site')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `agora sao ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/tmanhaa.png'
+        document.body.style.background = '#ffcfcd'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/ttarde.png'
+        document.body.style.background = '#df7e46'
+    } else {
+        img.src = 'imagens/tnoite.png'
+        document.body.style.background = '#4695df'
+
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+ 
