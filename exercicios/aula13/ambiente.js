1d80dada0ec007bb3e4b9f4a1f1ecf1e8ef655fd
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-03-09 17:18:35 -0300

    estudando desanimado

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,14 @@
+/*var c = 1
+while (c <= 6) {
+    console.log('tudo bem ?')
+    c++
+} */
+
+
+
+
+    var c = 1
+    do {
+        console.log(`passo ${c}`)
+        c++
+    } while (c <= 6) 
---
+++ exercicios/aula13/node_b120e47561d34.tmp.js
@@ -0,0 +1,4 @@
+var c = 6
+while (c <= 6) {
+    console.log('tudo bem ?')
+}
