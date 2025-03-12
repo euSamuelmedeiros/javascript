5f21d49e454f8def277b6d3df0ed5bc1f7103165
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-03-12 00:48:49 -0300

    estudando

--- desafios/003/script.js
+++ desafios/003/script.js
@@ -1,1 +1,25 @@
-alert('ola')
+function contar() {
+   let ini = document.getElementById('inc')
+   let fim = document.getElementById('Fim')
+   let passo = document.getElementById('ipss')
+   let res = document.getElementById('res')
+
+   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    window.alert('[ERRO] faltam dados')
+   } else  { 
+      res.innerText = 'contando:  ' 
+      let i = Number(ini.value)
+      let f = Number(fim.value)
+      let p = Number(passo.value)
+
+      for(let t = i; t <= f; t += p) {
+        res.innerHTML += `${t} `
+      }
+ }
+
+      
+      
+   } 
+
+
+
